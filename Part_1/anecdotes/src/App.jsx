a6e18a9e8d9a7ce7bb5be43c3a4abed96678bf41
Paralel <lucia.hudson.hernandez@gmail.com>
2026-01-23 20:18:05 +0100

    Functional exercise

--- Part_1/anecdotes/src/App.jsx
+++ Part_1/anecdotes/src/App.jsx
@@ -1,6 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 
+const TotalVotes = (votes) => {
+    return (
+        <div>
+            <p>{votes.selected} Has {votes.votes} votes</p>
+        </div>
+    )
+}
+
+const getMostVoted = (votes) => Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b)
+
+const MostVoted = ({anecdotes, votes}) => {
+    const mostVotedIndex = getMostVoted(votes)
+    return (
+        <div>
+            <p>{anecdotes[mostVotedIndex]}</p>
+            <p>Has {votes[mostVotedIndex]} votes</p>
+        </div>
+    )
+}
+
 const App = () => {
     const anecdotes = [
         'If it hurts, do it more often.',
@@ -12,12 +32,28 @@ const App = () => {
     ]
 
     const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState({0:0,1:0,2:0,3:0,4:0,5:0})
+    const mostvoted = getMostVoted(votes)
+    const copyVotes = {...votes}
 
     return (
         <div className="App">
+            <div className="Header1">Anecdote of the day</div>
             <p>{anecdotes[selected]}</p>
+
             <button onClick={() => setSelected (Math.floor(Math.random()*anecdotes.length))}>Next anecdote</button>
-        </div>
+            <button onClick={() => {
+                copyVotes[selected] += 1
+                setVotes(copyVotes)
+                console.log(votes)               
+            }}>Vote</button>
+
+            <div className="TotalVotes">Has a total votes of:</div>
+            <TotalVotes votes={copyVotes[selected]} />
+
+            <div className="Header1">Anecdote with most votes</div>     
+            <MostVoted anecdotes={anecdotes} votes={votes} MostVoted={mostvoted}/>   
+            </div>
     )
 }
 
