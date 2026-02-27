057437b55b2ec19743220d55141b47d76ec3d3ef
Paralel <lucia.hudson.hernandez@gmail.com>
2026-02-27 16:21:14 +0100

    C:\Users\lucia\OneDrive\Escritorio\HELSINKI\FULL-STACKopen\Part_1\Ejercicios usage of "map"

--- Part_2/src/App.jsx
+++ Part_2/src/App.jsx
@@ -1,19 +1,28 @@
 import './App.css'
-import Note from './components/note.jsx'
+import Course from './components/Course'
 
-const App = (props) => {
-  const {notes} = props
-  console.log(notes)
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-        <Note key={note.id} note={note} />)}
-      </ul>
-    </div>
-  )
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  return <Course course={course} />
 }
 
-export default App  
+export default App
