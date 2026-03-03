543aca8eb1e0d70810084642e4c8f3b4934b5e90
Paralel <lucia.hudson.hernandez@gmail.com>
2026-03-03 14:42:36 +0100

    Added new component

--- Part_2/src/App.jsx
+++ Part_2/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Course from './components/Course'
+import Course from './components/Course_list'
+import course from './components/Course_parts'
 
 const TotalHalf = ({ course }) => {
   const total = course[0].parts.reduce((sum, part) => sum + part.exercises, 0)
@@ -14,50 +15,6 @@ const TotalNode = ({course}) => {
 }
 
 const App = () => {
-  const course = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        { 
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
     return (
       <div>
         {course.map(course => <Course key={course.id} course={course} />)}
